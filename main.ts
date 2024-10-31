bd7919bdb4584647fd23eac72ec03276e9cb1f35
Aaron <gabbert.aaron@gmail.com>
2024-10-31 05:43:44 -0500

    added updateSites

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { CyberPlugin, getValidTld } from 'obsidian-cyber-utils';
 
 import { IOC_LENS_DEFAULT_SETTINGS, type IocLensSettings, IocLensSettingTab } from 'src/settings';
 import { DEFAULT_VIEW_TYPE, IndicatorSidebar } from 'src/iocLensView';
+import { defaultSites, type searchSite } from 'src/sites';
 
 // Remember to rename these classes and interfaces!
 
@@ -44,9 +45,25 @@ export default class IocLens extends CyberPlugin {
 
 	async loadSettings() {
 		this.settings = Object.assign({}, IOC_LENS_DEFAULT_SETTINGS, await this.loadData());
+		this.updateSites();
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	updateSites() {
+		const retval = [];
+		defaultSites.forEach(async (site: searchSite) => {
+			const settingSite = this.settings.searchSites.find(obj => obj.name === site.name);
+			const enabled = settingSite?.enabled ?? site.enabled;
+			const index = this.settings.searchSites.findIndex(obj => obj.name === site.name);
+			if (index >= 0) {
+				this.settings.searchSites[index] = {...site, enabled: enabled};
+			} else {
+				this.settings.searchSites.push({...site, enabled: enabled});
+			}
+			await this.saveSettings();
+		})
 	}
 }
