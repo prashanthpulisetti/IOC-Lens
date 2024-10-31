7d7a08bd31a5d4d1726fa37113cd0be31a70ecfd
Aaron <gabbert.aaron@gmail.com>
2024-10-31 05:45:14 -0500

    enhanced descriptions

--- src/settings.ts
+++ src/settings.ts
@@ -3,6 +3,7 @@ import { App, PluginSettingTab, Setting } from "obsidian";
 import { type CyberPluginSettings } from "obsidian-cyber-utils";
 
 import { defaultSites, type searchSite } from "./sites";
+import { DEFAULT_VIEW_TYPE } from "./iocLensView";
 
 export interface IocLensSettings extends CyberPluginSettings {
 }
@@ -30,22 +31,29 @@ export class IocLensSettingTab extends PluginSettingTab {
             .setHeading();
         defaultSites.forEach((site: searchSite) => {
         		const settingSite = this.plugin.settings.searchSites.find(obj => obj.name === site.name);
-        		const enabled = settingSite?.enabled ?? site.enabled;	
-            new Setting(containerEl)
-                .setName(site.name)
-                .setDesc(site.description ? site.description : "")
-                .addToggle(toggle => toggle
-                    .setValue(enabled)
-                    .onChange(async (value) => {
-                    		const index = this.plugin.settings.searchSites.findIndex(obj => obj.name === site.name);
-                    		if (index >= 0) {
-                    			this.plugin.settings.searchSites[index] = {...site, enabled: value};
-                    		} else {
-                    			this.plugin.settings.searchSites.push({...site, enabled: value});
-                    		}
-                        await this.plugin.saveSettings();
-                    })
-                )
+        		const enabled = settingSite?.enabled ?? site.enabled;
+                let desc = site.description ? site.description : "";
+                const supported = [];
+                if (site.ip) supported.push('IP');
+                if (site.domain) supported.push('domain');
+                if (site.hash) supported.push('file hash');
+                desc += `\nSupported IOC types: ${supported.join(', ')}`
+                new Setting(containerEl)
+                    .setName(site.name)
+                    .setDesc(desc)
+                    .addToggle(toggle => toggle
+                        .setValue(enabled)
+                        .onChange(async (value) => {
+                            const index = this.plugin.settings.searchSites.findIndex(obj => obj.name === site.name);
+                            if (index >= 0) {
+                                this.plugin.settings.searchSites[index] = {...site, enabled: value};
+                            } else {
+                                this.plugin.settings.searchSites.push({...site, enabled: value});
+                            }
+                            await this.plugin.saveSettings();
+                            this.plugin.sidebarContainers?.get(DEFAULT_VIEW_TYPE)
+                        })
+                    );
         });
     }
 }
